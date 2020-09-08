2d932ac73bb9bf8445114ce40e7ca3bfa9d3a532
jayakrishna123 <jayakrishnachadalawada29329@gmail.com>
2020-09-08 16:06:14 +0530

    my digital ocean

---
+++ build/precache-manifest.efbb42ece3917349b057b9447bc1951f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f091ff836060faed9a3ef4dfca0a456d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cb655c10e9894051511",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cb655c10e9894051511",
+    "url": "/static/js/main.e3b4176b.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
